3d3db4a858e820ab0c780c5bbbc1a3051f678fda
Guilherme Farias <guilherme@guiky.com.br>
2014-04-05 22:40:37 -0300

    Initializing index.js of app

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+api = require('./lib/api'),
+routes = require('./app/routes'),
+Router = require('./app/router'),
+app, port, apiPort, router;
+
+app = express();
+port = process.env.PORT || 3030;
+apiPort = process.env.API_PORT || 3031;
+router = new Router(routes);
+
+app.use(express.static(__dirname + '/public'));
+app.use(router.middleware());
+app.use('/api', api.proxyMiddleware(apiPort));
+app.listen(port);
+api.listen(apiPort);
